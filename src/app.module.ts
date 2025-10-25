ee588327b946e68de3381984d7b147e7970a1a6e
De1mosss <danilsbitnev@bk.ru>
2025-10-25 14:43:54 +0600

    feat: создал PrismaService и PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from "./prisma/prisma.module";
+import { PrismaService } from "./prisma/prisma.service";
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, UsersModule],
 })
 export class AppModule {}
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    async OnModuleInit() {
+        await this.$connect();
+    }
+
+    async OnModuleDestroy() {
+        await this.$disconnect();
+    }
+}
