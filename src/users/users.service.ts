061d8019184fd1fc961622cb58067661f91ee04b
De1mosss <danilsbitnev@bk.ru>
2025-10-25 15:38:11 +0600

    feat: реализована JWT авторизация и подключена защита маршрутов

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from "../prisma/prisma.service";
-import * as bcrypt from 'bcrypt'
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -12,10 +12,24 @@ export class UsersService {
             data: {
                 email,
                 password: hashedPassword,
-            }
-        })
+            },
+        });
     }
+
     async getAllUsers() {
-        return this.prisma.user.findMany();
+        return this.prisma.user.findMany({
+            select: {
+                id: true,
+                email: true,
+                createdAt: true,
+                updatedAt: true,
+            },
+        });
+    }
+
+    async findByEmail(email: string) {
+        return this.prisma.user.findUnique({
+            where: { email },
+        });
     }
 }
